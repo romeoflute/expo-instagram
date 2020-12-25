720faaa893be6991eb2fc294fe0c6d46ed372776
meoflauta <meoflauta@gmail.com>
2020-12-25 08:10:02 +0800

    Initial commit that includes React Navigation, Stack, Bottom and Top Tabs, and Redux libraries

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [liked, setLiked] = useState(false);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
