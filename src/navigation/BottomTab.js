bb77f0d0db6a83e7cdc7c8c63f9a34b80832d998
meoflauta <meoflauta@gmail.com>
2020-12-28 01:28:18 +0800

    Fetch user and add tab icons

--- src/navigation/BottomTab.js
+++ src/navigation/BottomTab.js
@@ -3,22 +3,73 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 
 import FeedTopTabs from './FeedTopTabs'
 import Explore from '../screens/Explore'
+import Add from '../screens/Add'
 import Notifications from '../screens/Notifications'
 import Profile from '../screens/Profile'
+
+import {MaterialCommunityIcons, MaterialIcons} from 'react-native-vector-icons'
+import { ColorPropType } from 'react-native';
 
 const BottomTabs = () => {
 
     const BottomTab = createMaterialBottomTabNavigator();
 
     return (
-        <BottomTab.Navigator>
+        <BottomTab.Navigator
+            activeColor="#000000"
+            inactiveColor="#a9a9a9"
+            barStyle={{ backgroundColor: '#d3d3d3' }}
+        >
             <BottomTab.Screen 
             name="Feed" 
             component={FeedTopTabs}
+            options={{
+                tabBarLabel: 'Feed',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="home" color={color} size={26} />
+                ),
+              }}
             />
-            <BottomTab.Screen name="Explore" component={Explore} />
-            <BottomTab.Screen name="Notifications" component={Notifications} />
-            <BottomTab.Screen name="Profile" component={Profile} />
+            <BottomTab.Screen 
+                name="Explore" 
+                component={Explore} 
+                options={{
+                    tabBarLabel: 'Discover',
+                    tabBarIcon: ({ color }) => (
+                      <MaterialIcons name="search" color={color} size={26} />
+                    ),
+                  }}
+            />
+            <BottomTab.Screen 
+                name="Add" 
+                component={Add} 
+                options={{
+                    tabBarLabel: 'Add',
+                    tabBarIcon: ({ color }) => (
+                      <MaterialCommunityIcons name="camera-plus" color={color} size={26} />
+                    ),
+                  }}
+            />
+            <BottomTab.Screen 
+                name="Notifications" 
+                component={Notifications} 
+                options={{
+                    tabBarLabel: 'Notifications',
+                    tabBarIcon: ({ color }) => (
+                      <MaterialCommunityIcons name="heart" color={color} size={26} />
+                    ),
+                  }}
+            />
+            <BottomTab.Screen 
+                name="Profile" 
+                component={Profile} 
+                options={{
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color }) => (
+                      <MaterialIcons name="person" color={color} size={26} />
+                    ),
+                  }}
+            />
         </BottomTab.Navigator>
     )
   }
